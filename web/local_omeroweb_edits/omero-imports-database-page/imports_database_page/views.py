32003ba5a33344965cb08d7f012aba78563a184b
Rodrigo Rosas-Bertolini <rodrigo.rosas3@gmail.com>
2024-10-22 16:04:35 +0200

    Splendidly passing the user id and also using the acceptance metabase server to display real logs

--- web/local_omeroweb_edits/omero-imports-database-page/imports_database_page/views.py
+++ web/local_omeroweb_edits/omero-imports-database-page/imports_database_page/views.py
@@ -12,14 +12,18 @@ logger = logging.getLogger(__name__)
 @login_required()
 @render_response()
 def imports_database_page(request, conn=None, **kwargs):
-    METABASE_SITE_URL = "http://localhost:3000"
+    METABASE_SITE_URL = "https://omero-acc.amc.nl/dashboard"
     METABASE_SECRET_KEY = "1f8e2c8ae6450b1035fe5ac9219a8f34702ab3cb01b1c3c767cfb55922c1d881"
+
+    # Get the current user's information
+    current_user = conn.getUser()
+    username = current_user.getName()
+    user_id = current_user.getId()
 
     payload = {
         "resource": {"dashboard": 2},
         "params": {
-            "user": None,
-            "group": None
+            "user": user_id,
         },
         "exp": int(time.time()) + (10 * 60)  # 10 minute expiration
     }
@@ -28,7 +32,9 @@ def imports_database_page(request, conn=None, **kwargs):
     context = {
         'metabase_site_url': METABASE_SITE_URL,
         'metabase_token': token,
-        'template': 'importsdatabase/webclient_plugins/imports_database_page.html'
+        'template': 'importsdatabase/webclient_plugins/imports_database_page.html',
+        'username': username,
+        'user_id': user_id
     }
     return context
 
